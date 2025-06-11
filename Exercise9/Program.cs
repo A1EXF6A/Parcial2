c67b7018ab8bc0a3e5f25867b51bbb5f3028f31b
RanMd <randommid1873@gmail.com>
2025-06-11 09:35:25 -0500

    chore: Initial commit Exercise 9

--- Exercise9/Program.cs
+++ Exercise9/Program.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Exercise9
+﻿namespace Exercise9
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+          System.Console.WriteLine("Hello, World!");
         }
     }
 }
